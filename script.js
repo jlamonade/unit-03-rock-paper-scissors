78383a7259067da8c7f5a104905d522e7fa8fdbf
Jason Lam <nyjasonlam@gmail.com>
2021-04-15 11:50:08 -0400

    Added pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // Dependencies (HTML Elements, Libraries, etc.) =====================
+
+
 // Starting Data =====================================================
+    // 1 initialize stats for the game wins/losses/ties
+
 // Functions =========================================================
+    // 4 choice is recorded
+    // 5 randomly generate number between 0 and 2
+    // 6 number determines computer choice of rock, paper, or scissors
+    // 7 check for win/loss/tie
+    // 10 if yes continue loop from begin round
+
 // User Interactions =================================================
+    // 3 prompt user to enter a choice of rock, paper, or scissors
+    // 8 alert user to result // display results
+    // 9 ask user if they want to play again
+
 // Initialization ====================================================
+    // 2 begin round
+
+
+
+// Pseudocode
+
+// initialize stats for the game wins/losses/ties
+// begin round
+// prompt user to enter a choice of rock, paper, or scissors
+// choice is recorded
+// randomly generate number between 0 and 2
+// number determines computer choice of rock, paper, or scissors
+// check for win/loss/tie
+// record outcome to stats
+// alert user to result
+// display results
+// ask user if they want to play again
+// if yes continue loop from begin round
